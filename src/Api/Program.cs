1d6d1f22a7000c71595869c0658b3f57c353e65b
Manuel Valenzuela <manuel_valenzuela@jabil.com>
2026-04-23 11:39:18 -0600

    feat: added version endpoint

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +16,22 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/version", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var assembly = Assembly.GetExecutingAssembly();
+    var infoVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+    .InformationalVersion;
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var fileVersion = assembly.GetName().Version?.ToString();
+
+    return Results.Ok(new
+    {
+        Version = infoVersion ?? fileVersion,
+        Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+        RunTime = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription,
+        ServerTime = DateTime.UtcNow
+    });
+}).WithName("Version")
+.WithTags("Version");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
